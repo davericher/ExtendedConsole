9e80a96380539032f00bf6798c290d4a1cc1fece
Dave Richer <dave@ir0ny.com>
2015-02-04 00:43:43 -0500

    Comments! oh and other things

--- ExtendedConsole/ExtConsole.cs
+++ ExtendedConsole/ExtConsole.cs
@@ -6,8 +6,17 @@ namespace ExtendedConsole
 {
     public class ExtConsole
     {
+        /// <summary>
+        /// The primary buffer, 
+        /// It keeps track of the current line, and each line contains a dictionary of strings and colors
+        /// </summary>
         private readonly Dictionary<int, Dictionary<string, ConsoleColor>> _tmpConsoleBuffer;
+       
         // Constructors
+        /// <summary>
+        /// Constructor requiring the title of the console app
+        /// </summary>
+        /// <param name="title">The title of the current application</param>
         public ExtConsole(string title)
         {
             _tmpConsoleBuffer = new Dictionary<int, Dictionary<string, ConsoleColor>>();
@@ -16,24 +25,36 @@ namespace ExtendedConsole
             InitWindow();
         }
 
-        public ExtConsole()
+        /// <summary>
+        /// No Paramater constructor which will set default Console title
+        /// </summary>
+        public ExtConsole() : this(DefaultWindowTitle)
         {
-            InitWindow();
         }
 
-        // Auto Properties
-        public ConsoleColor RuleOnColor { get; set; }
-        public ConsoleColor RuleOffColor { get; set; }
-        public ConsoleColor PrimaryColor { get; set; }
-        public ConsoleColor BoldColor { get; set; }
-        public ConsoleColor ErrorColor { get; set; }
-        public ConsoleColor SuccessColor { get; set; }
-        public int WindowWidth { get; set; }
-        public int WindowHeight { get; set; }
-        public char RuleChar { get; set; }
-        public int ElementsPerLine { get; set; }
-        public string WindowTitle { get; set; }
 
+        // Constants
+        private const string DefaultWindowTitle = "Console Application";
+
+        // Auto Properties
+        public ConsoleColor RuleOnColor { get; set; }       // On color for Horizontal Line and header line
+        public ConsoleColor RuleOffColor { get; set; }      // Off color for Horizontal line and header line
+        public ConsoleColor PrimaryColor { get; set; }      // Primary text color
+        public ConsoleColor BoldColor { get; set; }         // Primary bold color
+        public ConsoleColor ErrorColor { get; set; }        // Primary error color
+        public ConsoleColor SuccessColor { get; set; }      // Primary success color
+        public int WindowWidth { get; set; }                // Window width
+        public int WindowHeight { get; set; }               // Window Height
+        public char RuleChar { get; set; }                  // Horizontal line / header line display character
+        public int ElementsPerLine { get; set; }            // Amount of elements per line in PrintArray TODO this does not need to be here
+        public string WindowTitle { get; set; }             // Title of the window
+        public string PausePrompt { get; set; }             // Default pause prompt
+
+        /// <summary>
+        /// Initialize the defaults, 
+        /// extend this class and override this method for different defaults
+        /// or change them pragmatically during execution
+        /// </summary>
         public void LoadDefaults()
         {
             RuleOnColor = ConsoleColor.Red;
@@ -46,24 +67,37 @@ namespace ExtendedConsole
             WindowHeight = 50;
             RuleChar = '*';
             ElementsPerLine = 10;
+            PausePrompt = "[ Press Any Key to Continue ]";
         }
 
+        /// <summary>
+        /// Adjust the console size specified by the properties WindowWidth and WindowHeight
+        /// </summary>
         public void SetWindowSize()
         {
             Console.SetWindowSize(WindowWidth, WindowHeight);
         }
 
+        /// <summary>
+        /// Set the window title based on the property WindowTitle
+        /// </summary>
         public void SetWindowTitle()
         {
             Console.Title = WindowTitle;
         }
 
+        /// <summary>
+        /// Reinitialize the window, calling on SetWindowSize and SetWindowTitle
+        /// </summary>
         public void InitWindow()
         {
             SetWindowSize();
             SetWindowTitle();
         }
 
+        /// <summary>
+        /// Print the current buffer to the console, then clear it
+        /// </summary>
         public void Print()
         {
             foreach (var result in _tmpConsoleBuffer.SelectMany(results => results.Value))
@@ -75,12 +109,21 @@ namespace ExtendedConsole
             _tmpConsoleBuffer.Clear();
         }
 
+        /// <summary>
+        /// Check to see if there is anything in the buffer
+        /// If there are contents, dumps it to the console
+        /// </summary>
         public void PrintIfExists()
         {
             if (_tmpConsoleBuffer.Any())
                 Print();
         }
 
+        /// <summary>
+        /// Write into the buffer
+        /// </summary>
+        /// <param name="text">The text to be written</param>
+        /// <param name="color">The ConsoleColor you wish the text to be displayed in</param>
         public void Write(string text, ConsoleColor color)
         {
             var count = !_tmpConsoleBuffer.Any() ? 0 : _tmpConsoleBuffer.Count() + 1;
@@ -88,42 +131,76 @@ namespace ExtendedConsole
             _tmpConsoleBuffer.Add(count, line);
         }
 
+        /// <summary>
+        /// See Above, overloaded for one argument with default color set
+        /// </summary>
+        /// <param name="text">The text to be written</param>
         public void Write(string text)
         {
             Write(text, PrimaryColor);
         }
 
+        /// <summary>
+        /// See Write, add a linebreak
+        /// </summary>
+        /// <param name="text">Text to be written</param>
+        /// <param name="color">ConsoleColor of text</param>
         public void WriteLine(string text, ConsoleColor color)
         {
-            Write(text + "\n", color);
+            Write(text + Environment.NewLine, color);
         }
-
+       
+        /// <summary>
+        /// See Write
+        /// </summary>
+        /// <param name="text">Text to be written in default color</param>
         public void WriteLine(string text)
         {
             WriteLine(text, PrimaryColor);
         }
 
+        /// <summary>
+        /// See Write
+        /// </summary>
+        /// <param name="text">Text to be written in bold color</param>
         public void WriteBold(string text)
         {
             Write(text, BoldColor);
         }
 
+        /// <summary>
+        /// See WriteLine
+        /// </summary>
+        /// <param name="text">Line to be written in bold</param>
         public void WriteLineBold(string text)
         {
             WriteLine(text, BoldColor);
         }
 
+        /// <summary>
+        /// Clear the current buffer
+        /// </summary>
         public void ClearBuffer()
         {
             _tmpConsoleBuffer.Clear();
         }
 
+        /// <summary>
+        /// Insert a linebreak into the buffer
+        /// </summary>
+        /// <param name="total">The amount of linesbreaks you want to insert, defaults to 1</param>
         public void LineBreak(int total = 1)
         {
             for (var counter = 0; counter < total; counter++)
-                Write("\n");
+                Write(Environment.NewLine);
         }
 
+        /// <summary>
+        /// Inser a horizontal line into the buffer
+        /// works by alternating betten RuleOnColor and RuleOffColor with RuleChar as the character
+        /// if you want consistent color set both to same ConsoleColor value
+        /// TODO Optional switch to disable color alteration and default on if it should be on or off
+        /// </summary>
         public void HorizontalLine()
         {
             for (var x = 1; x <= Console.WindowWidth - 1; x++)
@@ -131,11 +208,18 @@ namespace ExtendedConsole
             LineBreak();
         }
 
+        /// <summary>
+        /// Prompt the user to press Any Key to continue, uses PausePrompt as default prompt
+        /// </summary>
         public void Pause()
         {
-            Pause("[ Press Any Key to Continue ]");
+            Pause(PausePrompt);
         }
 
+        /// <summary>
+        /// See Pause
+        /// </summary>
+        /// <param name="text">The display prompt</param>
         public void Pause(string text)
         {
             PrintIfExists();
@@ -146,6 +230,15 @@ namespace ExtendedConsole
             LineBreak(2);
         }
 
+        /// <summary>
+        /// Get a yes or a no (y/n) from the user
+        /// will not break until at least one is pressed
+        /// </summary>
+        /// <param name="text">
+        ///     The optional Display prompt
+        ///     Default tp null and is appended by [y]es pr [n]o
+        /// </param>
+        /// <returns></returns>
         public bool ReadYesOrNo(String text = null)
         {
             ConsoleKeyInfo key;
@@ -164,22 +257,33 @@ namespace ExtendedConsole
 
             do
                 key = Console.ReadKey(true); 
-            while (!key.KeyChar.ToString().ToLower().Equals("y") && !key.KeyChar.ToString().ToLower().Equals("n"));
+            while (!Helpers.KeyIs(key,'y') && !Helpers.KeyIs(key,'n'));
 
             LineBreak(2);
 
-            return key.KeyChar.ToString().ToLower().Equals("y");
+            return Helpers.KeyIs(key,'y');
         }
 
+        /// <summary>
+        /// header lines are used to help format WriteHeader
+        /// RuleChar(RuleCharOff) Padding text Padding RuleChar(RuleCharOn)
+        /// </summary>
+        /// <param name="text">Text to be formated</param>
+        /// <param name="color">ConsoleColor of text</param>
         public void WriteAHeaderLine(string text, ConsoleColor color)
         {
             Write(RuleChar.ToString(), RuleOnColor);
-            var output = CenteredString(text, -1);
+            var output = Helpers.CenteredString(text, -1);
             Write(output, color);
             Write(RuleChar.ToString().PadLeft((Console.WindowWidth - 1) - output.Length - 1), RuleOnColor);
             LineBreak();
         }
 
+        /// <summary>
+        /// Combines WriteAHeaderLine and WriteAHorizontalLine to
+        /// display a padded formated header
+        /// </summary>
+        /// <param name="text">The text to be formatted</param>
         public void WriteHeader(string text)
         {
             HorizontalLine();
@@ -188,17 +292,20 @@ namespace ExtendedConsole
             LineBreak();
         }
 
+        /// <summary>
+        /// Write the current window title as a header
+        /// </summary>
         public void WriteTitle()
         {
             WriteHeader(WindowTitle);
         }
 
-        public String CenteredString(string text, int offset = 0)
-        {
-            return String.Format("{0," + (Console.WindowWidth/2 - offset + (text.Length/2)) + "}", text);
-        }
 
-        public void WriteAnIntArray(IEnumerable<int> arr)
+        /// <summary>
+        /// Write a formated array, allows for generics
+        /// </summary>
+        /// <param name="arr">Array to be printed</param>
+        public void WriteAnIntArray(IEnumerable<object> arr)
         {
             var counter = 0;
 
@@ -212,6 +319,13 @@ namespace ExtendedConsole
             LineBreak();
         }
 
+
+        /// <summary>
+        /// Read a vallid integer from the console
+        /// </summary>
+        /// <param name="prompt">Prompt to be displayed, defaults to "Please enter a number"</param>
+        /// <param name="error">Error message if non number is entered, default sto "That was not a valid number"</param>
+        /// <returns></returns>
         public int ReadAValidatedInt(
             String prompt = "Please enter a number: ",
             String error = "That was not a valid Number"
@@ -231,6 +345,12 @@ namespace ExtendedConsole
             return output;
         }
 
+        /// <summary>
+        /// Write a error line to the console formated
+        /// with line breaks and in ErrorColor
+        /// and of course beep
+        /// </summary>
+        /// <param name="text">Text to be formatted</param>
         public void WriteAErrorLine(string text)
         {
             LineBreak();
@@ -240,6 +360,12 @@ namespace ExtendedConsole
             Console.Beep();
         }
 
+        /// <summary>
+        /// Write a success line to the console formated
+        /// with line breaks and in SuccessColor
+        /// and of course a beep
+        /// </summary>
+        /// <param name="text"></param>
         public void WriteASuccess(string text)
         {
             LineBreak();
@@ -249,6 +375,12 @@ namespace ExtendedConsole
             Console.Beep(2600, 30);
         }
 
+        /// <summary>
+        /// Read a non empty string from the console
+        /// </summary>
+        /// <param name="prompt">Display prompt</param>
+        /// <param name="error">Optional error prompt default to Invalid Input</param>
+        /// <returns></returns>
         public string ReadANonEmptyString(string prompt, string error = "Invalid Input")
         {
             string input = null;
---
+++ ExtendedConsole/Helpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExtendedConsole
+{
+    public static class Helpers
+    {
+        /// <summary>
+        /// Check the current read key for the specified character
+        /// </summary>
+        /// <param name="press">ConsoleKeyInfo - read by Console.ReadKey()</param>
+        /// <param name="key">The character you are checking for</param>
+        /// <returns>True if matched, False if not</returns>
+        public static bool KeyIs(ConsoleKeyInfo press, char key)
+        {
+            return press.KeyChar.ToString().ToLower().Equals(key.ToString());
+        }
+
+        /// <summary>
+        /// Right pad a line of text so it is centered on the console and takes into account 
+        /// current resolution of said console
+        /// </summary>
+        /// <param name="text">The text to be centered</param>
+        /// <param name="offset">The optional negative offset, defaults to 0</param>
+        /// <returns>A reformatted string</returns>
+        public static String CenteredString(string text, int offset = 0)
+        {
+            return String.Format("{0," + (Console.WindowWidth / 2 - offset + (text.Length / 2)) + "}", text);
+        }
+    }
+}
