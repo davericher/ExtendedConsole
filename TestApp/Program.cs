9d57aab05c7e14ebe6d24c4e8f81ac245a89c42b
Dave Richer <dave@ir0ny.com>
2015-02-11 20:30:15 -0500

    Added GetAValidDouble and OptionLists

---
+++ TestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtendedConsole;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtConsole c = new ExtConsole();
+            c.PenisLine();
+            c.Print();
+            c.Pause();
+        }
+    }
+}
